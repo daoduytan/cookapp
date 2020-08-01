6ebe21a903b5c2dc36656c67db49d14975687408
daoduytan <ddtan2212@gmail.com>
2020-08-01 09:43:42 +0700

    Updates

---
+++ precache-manifest.2ce34362ee80ee1e303e37a908017266.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "8daefe2b4e7bd059812c",
+    "url": "/cookapp/static/js/app.5afe2eaa.chunk.js"
+  },
+  {
+    "revision": "b9df450fb1dd2c95c91c",
+    "url": "/cookapp/static/js/runtime~app.d1296147.js"
+  },
+  {
+    "revision": "c775440a7bf7a6526900",
+    "url": "/cookapp/static/js/2.f6dde411.chunk.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/cookapp/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/cookapp/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "188ca708e4cbbbe3837c3d904b8eeb61",
+    "url": "/cookapp/./fonts/Quicksand_300Light.ttf"
+  },
+  {
+    "revision": "316c232187bac46e708fad0b5d83666f",
+    "url": "/cookapp/./fonts/Quicksand_400Regular.ttf"
+  },
+  {
+    "revision": "75365efdb746b3e14b37b4ab21fa6ed3",
+    "url": "/cookapp/./fonts/Quicksand_500Medium.ttf"
+  },
+  {
+    "revision": "0ced4a3b332e6ea97d30fa137cee464f",
+    "url": "/cookapp/./fonts/Quicksand_600SemiBold.ttf"
+  },
+  {
+    "revision": "3cd49fcca0fcaa042649d15bab54199e",
+    "url": "/cookapp/./fonts/Quicksand_700Bold.ttf"
+  },
+  {
+    "revision": "9da58588c7999d67a2fe421727bcbd3f",
+    "url": "/cookapp/static/media/an-sang.9da58588.jpg"
+  },
+  {
+    "revision": "d0ff5dd10bc2fc8259d37e3cfca44174",
+    "url": "/cookapp/static/media/an-trua.d0ff5dd1.png"
+  },
+  {
+    "revision": "f989c8e140f530466812c4807fbab765",
+    "url": "/cookapp/static/media/an-toi.f989c8e1.jpg"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/cookapp/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cookapp/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/cookapp/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/cookapp/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/cookapp/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/cookapp/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/cookapp/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/cookapp/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/cookapp/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/cookapp/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/cookapp/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/cookapp/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/cookapp/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/cookapp/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/cookapp/favicon.ico"
+  },
+  {
+    "revision": "f1ba67aae3b6966da1dd349512cd8114",
+    "url": "/cookapp/index.html"
+  },
+  {
+    "revision": "7c4cb5a47b2b97e07ff4b976ddfdac9a",
+    "url": "/cookapp/manifest.json"
+  },
+  {
+    "revision": "2ec4653722d12d250bf65901775654bd",
+    "url": "/cookapp/register-service-worker.js"
+  }
+];
